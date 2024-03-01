17775b62b353d134976dca50438d0bce4a9473d1
WriterV2 <primary-v2@protonmail.com>
2024-03-01 21:24:50 +0100

    Init

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use anyhow::Ok;
+use sqlx::sqlite::SqlitePool;
+use std::env;
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> anyhow::Result<()> {
+    let pool = SqlitePool::connect(&env::var("DATABASE_URL")?).await?;
+
+    Ok(())
+}
