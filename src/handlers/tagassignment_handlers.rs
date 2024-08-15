1f37a46b2438ac681e4efdeddebea4994d602c67
WriterV2 <primary-v2@protonmail.com>
2024-08-15 14:39:07 +0200

    Feat: Extend tag assignment API route by ability to add multiple tags at once

--- src/handlers/tagassignment_handlers.rs
+++ src/handlers/tagassignment_handlers.rs
@@ -3,6 +3,7 @@ use axum::response::IntoResponse;
 use axum::{Extension, Json};
 use axum_extra::extract::Query;
 use serde::Deserialize;
+use sqlx::{QueryBuilder, Sqlite};
 
 use crate::error::AppError;
 use crate::tagassignment::TagAssignment;
@@ -11,7 +12,7 @@ use super::ApiContext;
 
 #[derive(Deserialize, Debug)]
 pub struct TagAssignmentPostQueryParams {
-    tag_id: i64,
+    tag_id: Vec<i64>,
     booking_id: i64,
 }
 
@@ -19,13 +20,22 @@ pub async fn post_tagassignment(
     ctx: Extension<ApiContext>,
     Query(params): Query<TagAssignmentPostQueryParams>,
 ) -> Result<impl IntoResponse, AppError> {
-    let tagassignment = sqlx::query_as!(
-        TagAssignment,
-        "INSERT INTO tagassignment (tgid, bid) VALUES ($1, $2) RETURNING tgid, bid",
-        params.tag_id,
-        params.booking_id,
-    )
-    .fetch_one(&ctx.pool)
-    .await?;
-    Ok((StatusCode::CREATED, Json(tagassignment)))
+    let mut tx = ctx.pool.begin().await?;
+    let mut query_builder: QueryBuilder<Sqlite> =
+        QueryBuilder::new("INSERT INTO tagassignment (tgid, bid) VALUES");
+
+    for (i, tid) in params.tag_id.iter().enumerate() {
+        query_builder.push(" (");
+        query_builder.push_bind(tid);
+        query_builder.push(", ");
+        query_builder.push_bind(params.booking_id);
+        query_builder.push(")");
+        if params.tag_id.len() - 1 > i {
+            query_builder.push(",");
+        }
+    }
+    query_builder.push(" RETURNING tgid, bid;");
+    let result: Vec<TagAssignment> = query_builder.build_query_as().fetch_all(&mut *tx).await?;
+    tx.commit().await?;
+    Ok((StatusCode::CREATED, Json(result)))
 }
